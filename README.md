ab20c39ea263f524d49d85d383f3b4499e17a463
Manuel Sardi <manuel.sardi@dnv.com>
2025-07-02 12:49:33 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,25 +1,22 @@
 chdump
 ======
+This is a modified version of the [original chdump tool written by runreveal](https://github.com/runreveal/chdump), tuned specifically for the schema export of Clickhouse replicated clusters instead of single servers.  
 
-This tool dumps the schema from the database given in the connection string or
-the default database.  It does not dump or export any data contained in those
-tables.
+This tool dumps the schema from all databases on a ClickHouse cluster (with an option to exclude the default/system databases). It does not dump or export any data contained in those
+tables. It will gather all the databases in the cluster, crawl through them and generate a CREATE query for the database and all the tables and views present.
 
-This is useful for cloning database schemas between instances or deployments of
-clickhouse.
+Since this version of the tool is tuned to work with replicated clusters, the option 'ON CLUSTER [cluster]' will be appended to all CREATE statements. The 'IF NOT EXISTS' option is also used for idempotence purposes. 
 
-There are existing tools like `clickhouse-backup` which read the metadata
-directories to achieve the same thing.  However, that tool (and other similar
-tools) require filesystem access to the deployed database which isn't the case
-if you're running clickhouse cloud or interacting with other hosted clickhouse
-databases.
+This is useful for cloning database schemas between instances or deployments of clickhouse.
+
+There are existing tools like `clickhouse-backup` which read the metadata directories to achieve the same thing.  However, that tool (and other similar tools) require filesystem access to the deployed database which isn't the case if you're running clickhouse cloud or interacting with other hosted clickhouse databases.
 
 ## Usage
 
 To dump the schema, run the command like so:
 
 ```
-chdump 'clickhouse://username:password@hostname[:port]/[database][?setting=value][&setting2=value]'
+chdump --cluster-name new-cluster --exclude-default --conn-string 'clickhouse://username:password@hostname[:port]/[database][?setting=value][&setting2=value]'
 ```
 
 It spits the schema to standard output, with table definitions separated by a
--- cmd/chdump/main.go
+++ cmd/chdump/main.go
@@ -3,26 +3,38 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"flag"
 	"log"
-	"os"
+	"slices"
+	"strings"
 
 	_ "github.com/ClickHouse/clickhouse-go/v2"
 )
 
 func main() {
-	// Replace these with your actual database credentials and connection details
-	const (
-		address  = "tcp://localhost:9000"
-		username = "default"
-		password = ""
-		database = "default"
-	)
 
-	clickhouseDSN := os.Args[1]
+	excludeDefaultDBs := flag.Bool("exclude-default", true, "Exclude default and system DBs")
+	clusterName := flag.String("cluster-name", "", "Name of the target cluster")
+	clickhouseDSN := flag.String("conn-string", "", "The ClickHouse connection string")
+	flag.Parse()
+	
+
+	systemDBs := []string{
+		"INFORMATION_SCHEMA",
+		"default",
+		"information_schema",
+		"system",
+	}
+
+	if *clickhouseDSN == "" {
+		log.Fatalf("Missing required argument conn-string")
+	}
+	if *clusterName == "" {
+		log.Fatalf("Missing required argument cluster-name")
+	}
 
 	// Establish a connection to the database
-	// connStr := fmt.Sprintf("%s/%s?username=%s&password=%s", address, database, username, password)
-	db, err := sql.Open("clickhouse", clickhouseDSN)
+	db, err := sql.Open("clickhouse", *clickhouseDSN)
 	if err != nil {
 		log.Fatalf("Failed to open database: %v", err)
 	}
@@ -32,37 +44,117 @@ func main() {
 	}
 	defer db.Close()
 
-	// Query to retrieve table names in the specified database
-	tableNamesQuery := "SHOW TABLES"
-	rows, err := db.Query(tableNamesQuery)
+	// If all databases are wanted, query for them
+	var databases []string
+	databaseNamesQuery := "SHOW DATABASES"
+	rows, err := db.Query(databaseNamesQuery)
 	if err != nil {
 		log.Fatalf("Failed to execute query: %v", err)
 	}
 	defer rows.Close()
-
-	var tableName string
+	var databaseName string
 	for rows.Next() {
-		if err := rows.Scan(&tableName); err != nil {
+		if err := rows.Scan(&databaseName); err != nil {
 			log.Fatalf("Failed to scan row: %v", err)
 		}
-		// Query to retrieve the DDL of each table
-		showCreateTableQuery := fmt.Sprintf("SHOW CREATE TABLE `%s`", tableName)
-		tableDDL, err := db.Query(showCreateTableQuery)
-		if err != nil {
-			log.Fatalf("Failed to get DDL for table %s: %v", tableName, err)
-		}
-		defer tableDDL.Close()
-
-		if tableDDL.Next() {
-			var ddl string
-			if err := tableDDL.Scan(&ddl); err != nil {
-				log.Fatalf("Failed to scan DDL of table %s: %v", tableName, err)
+		// Exclude default and system databases
+		if (*excludeDefaultDBs) {
+			// Append only non-default databases
+			if !(slices.Contains(systemDBs, databaseName)) {
+				databases = append(databases, databaseName)
 			}
-			fmt.Printf("%s\n;\n----------------------------------------\n", ddl)
+		} else {
+			// Append all
+			databases = append(databases, databaseName)
 		}
 	}
 
-	if err := rows.Err(); err != nil {
-		log.Fatalf("Error during row iteration: %v", err)
+
+	for _, databaseName := range databases {
+		newDSN := fmt.Sprintf("%s/%s", *clickhouseDSN, databaseName)
+		db, err := sql.Open("clickhouse", newDSN)
+		if err != nil {
+			log.Fatalf("Failed to open database: %v", err)
+		}
+		// Ping the database to ensure connection is established
+		if err := db.Ping(); err != nil {
+			log.Fatalf("Failed to connect: %v", err)
+		}
+		defer db.Close()
+
+		// Query to retrieve the DDL of each database
+		showCreateDatabaseQuery := fmt.Sprintf("SHOW CREATE DATABASE `%s`", databaseName)
+		databaseDDL, err := db.Query(showCreateDatabaseQuery)
+		if err != nil {
+			log.Fatalf("Failed to get DDL for database %s: %v", databaseName, err)
+		}
+
+		if databaseDDL.Next() {
+			var ddl string
+			if err := databaseDDL.Scan(&ddl); err != nil {
+				log.Fatalf("Failed to scan DDL of database %s: %v", databaseName, err)
+			}
+			// New version for printing database definitions - append the ON CLUSTER clause to create tables on all replicas
+			//fmt.Printf("%s\n;\n----------------------------------------\n", ddl)
+			ddl = strings.Replace(ddl, "CREATE DATABASE", "CREATE DATABASE IF NOT EXISTS", 1)
+			for i, line := range strings.Split(strings.TrimSuffix(ddl, "\n"), "\n") {
+				fmt.Println(line)
+				if (i == 0) {
+					fmt.Printf("ON CLUSTER %s\n", *clusterName)
+				}
+			}
+			fmt.Printf("\n;\n----------------------------------------\n")
+		}
+		// Set the database to use in the resulting sql file
+		fmt.Printf("USE %s\n;\n----------------------------------------\n", databaseName)
+		defer databaseDDL.Close()
+
+		// Query to retrieve table names in the specified database
+		tableNamesQuery := "SHOW TABLES"
+		rows, err := db.Query(tableNamesQuery)
+		if err != nil {
+			log.Fatalf("Failed to execute query: %v", err)
+		}
+		defer rows.Close()
+
+		var tableName string
+		for rows.Next() {
+			if err := rows.Scan(&tableName); err != nil {
+				log.Fatalf("Failed to scan row: %v", err)
+			}
+			// Query to retrieve the DDL of each table
+			showCreateTableQuery := fmt.Sprintf("SHOW CREATE TABLE `%s`", tableName)
+			tableDDL, err := db.Query(showCreateTableQuery)
+			if err != nil {
+				log.Fatalf("Failed to get DDL for table %s: %v", tableName, err)
+			}
+			defer tableDDL.Close()
+
+			if tableDDL.Next() {
+				var ddl string
+				if err := tableDDL.Scan(&ddl); err != nil {
+					log.Fatalf("Failed to scan DDL of table %s: %v", tableName, err)
+				}
+				// New version for printing table definitions - append the ON CLUSTER clause to create tables on all replicas
+				//fmt.Printf("%s\n;\n----------------------------------------\n", ddl)
+
+				// Add the IF NOT EXISTS on each CREATE for idempotence
+				ddl = strings.Replace(ddl, "CREATE TABLE", "CREATE TABLE IF NOT EXISTS", 1)
+				ddl = strings.Replace(ddl, "CREATE VIEW", "CREATE VIEW IF NOT EXISTS", 1)
+				ddl = strings.Replace(ddl, "CREATE MATERIALIZED VIEW", "CREATE MATERIALIZED VIEW IF NOT EXISTS", 1)
+
+				for i, line := range strings.Split(strings.TrimSuffix(ddl, "\n"), "\n") {
+					fmt.Println(line)
+					if (i == 0) {
+						fmt.Printf("ON CLUSTER %s\n", *clusterName)
+					}
+				}
+				fmt.Printf("\n;\n----------------------------------------\n")
+			}
+		}
+
+		if err := rows.Err(); err != nil {
+			log.Fatalf("Error during row iteration: %v", err)
+		}
 	}
-}
+} 
